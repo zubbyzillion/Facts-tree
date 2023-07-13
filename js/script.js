cd2952d415eb91e2cbef83e763dc17c7ddaf6fa9
zubbyzillion <princewillbeluonwuogbo1@gmail.com>
2023-07-13 13:33:52 +0100

    mapped through existin data

--- js/script.js
+++ js/script.js
@@ -1,7 +1,55 @@
-/*
+const initialFacts = [
+  {
+    id: 1,
+    text: "React is being developed by Meta (formerly facebook)",
+    source: "https://opensource.fb.com/",
+    category: "technology",
+    votesInteresting: 24,
+    votesMindblowing: 9,
+    votesFalse: 4,
+    createdIn: 2021,
+  },
+  {
+    id: 2,
+    text: "Millennial dads spend 3 times as much time with their kids than their fathers spent with them. In 1982, 43% of fathers had never changed a diaper. Today, that number is down to 3%",
+    source:
+      "https://www.mother.ly/parenting/millennial-dads-spend-more-time-with-their-kids",
+    category: "society",
+    votesInteresting: 11,
+    votesMindblowing: 2,
+    votesFalse: 0,
+    createdIn: 2019,
+  },
+  {
+    id: 3,
+    text: "Lisbon is the capital of Portugal",
+    source: "https://en.wikipedia.org/wiki/Lisbon",
+    category: "society",
+    votesInteresting: 8,
+    votesMindblowing: 3,
+    votesFalse: 1,
+    createdIn: 2015,
+  },
+];
+
+// Selecting DOM elements
 const btn = document.querySelector(".btn-open");
 const form = document.querySelector(".fact-form");
+const factsList = document.querySelector(".facts-list");
 
+//Creating DOM elements:Render facts in list
+factsList.innerHTML = "";
+
+// factsList.insertAdjacentHTML("afterbegin", "<li>Zubby</li>");
+
+const htmlArr = initialFacts.map(
+  (fact) => `<li class="fact">${fact.text}</li>`);
+
+console.log(htmlArr);
+const html = htmlArr.join("");
+factsList.insertAdjacentHTML("afterbegin", html);
+
+// Toggle form visibility
 btn.addEventListener("click", function () {
     if (form.classList.contains("hidden")) {
         form.classList.remove("hidden");
@@ -12,6 +60,7 @@ btn.addEventListener("click", function () {
     }
 });
 
+/*
 // Functions
 
 function calcYear(year) {
@@ -66,6 +115,7 @@ console.log(calcFactAge2(2015));
 
 // Array
 
+/*
 const fact = ["Lisbon is the  capital of Portugal", 2015, true];
 // console.log(fact);
 console.log(2);
@@ -105,6 +155,7 @@ console.log(factObj.createSummary()); // Calling Methods with functions in it.
 //     return el * 10;
 // });
 
+/*
 const times10 = [2, 4, 6, 8].map((el) => el * 10);
 
 console.log(times10);
@@ -170,3 +221,4 @@ const CATEGORIES = [
   const factAges = initialFacts.map((el) => calcYear(el.createdIn));
   console.log(factAges);
   console.log(factAges.join(" & "));
+  */
