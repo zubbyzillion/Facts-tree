5fe6cd4f75abd4add941a6656e397855e8cc2cb9
zubbyzillion <princewillbeluonwuogbo1@gmail.com>
2023-07-14 04:31:31 +0100

    Integrated the backend to the frontend

--- js/script.js
+++ js/script.js
@@ -41,14 +41,19 @@ const factsList = document.querySelector(".facts-list");
 factsList.innerHTML = "";
 
 // Load data from Supabase
-const res = fetch("https://ywrkgrgyvxhhejztanpt.supabase.co/rest/v1/Facts", {
-  headers: {
-    apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl3cmtncmd5dnhoaGVqenRhbnB0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODY3MTkwMzAsImV4cCI6MjAwMjI5NTAzMH0.Vlj8SXhL2_KbF9VsypPX44w8fsKra5GCgIAz3C6qX2o",
-    authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl3cmtncmd5dnhoaGVqenRhbnB0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODY3MTkwMzAsImV4cCI6MjAwMjI5NTAzMH0.Vlj8SXhL2_KbF9VsypPX44w8fsKra5GCgIAz3C6qX2o",
-  },
-});
+loadFacts();
 
-console.log(res);
+async function loadFacts() {
+  const res = await fetch("https://ywrkgrgyvxhhejztanpt.supabase.co/rest/v1/Facts", {
+    headers: {
+      apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl3cmtncmd5dnhoaGVqenRhbnB0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODY3MTkwMzAsImV4cCI6MjAwMjI5NTAzMH0.Vlj8SXhL2_KbF9VsypPX44w8fsKra5GCgIAz3C6qX2o",
+      authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl3cmtncmd5dnhoaGVqenRhbnB0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODY3MTkwMzAsImV4cCI6MjAwMjI5NTAzMH0.Vlj8SXhL2_KbF9VsypPX44w8fsKra5GCgIAz3C6qX2o",
+    },
+  })
+  const data = await res.json();
+  createFactsList(data);
+};
+
 
 // createFactsList(initialFacts);
 
