afe5ab0f39f8df60118bbca7940d44e0ba444982
zubbyzillion <princewillbeluonwuogbo1@gmail.com>
2023-07-27 05:21:20 +0100

    Commit

--- facts-tree/src/App.js
+++ facts-tree/src/App.js
@@ -1,5 +1,13 @@
 function App() {
-  return <h1> Hello World! </h1>
+  return (
+    <header class="header">
+      <div class="logo">
+          <img src="logo.png" alt="Facts Tree" />
+          <h1>Facts Tree</h1>
+      </div>
+        <button class="btn btn-large btn-open">Share a Fact</button>
+    </header>
+  );
 }
 
 export default App;
