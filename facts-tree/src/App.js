c8b4b82c2009062436a03641774f0aac67570ce8
zubbyzillion <princewillbeluonwuogbo1@gmail.com>
2023-07-29 04:29:21 +0100

    Added Font Linksin React File

--- facts-tree/src/App.js
+++ facts-tree/src/App.js
@@ -1,11 +1,13 @@
+import "./style.css";
+
 function App() {
   return (
-    <header class="header">
-      <div class="logo">
+    <header className="header">
+      <div className="logo">
           <img src="logo.png" alt="Facts Tree" />
           <h1>Facts Tree</h1>
       </div>
-        <button class="btn btn-large btn-open">Share a Fact</button>
+        <button className="btn btn-large btn-open">Share a Fact</button>
     </header>
   );
 }
