5e64c906b5d9c57efcac456d624ad8b9d75add95
zubbyzillion <princewillbeluonwuogbo1@gmail.com>
2023-07-29 05:06:42 +0100

    Divided UI into Component

--- facts-tree/src/App.js
+++ facts-tree/src/App.js
@@ -1,15 +1,40 @@
 import "./style.css";
 
 function App() {
+  const appTitle = "Fact Tree";
+
   return (
-    <header className="header">
-      <div className="logo">
-          <img src="logo.png" alt="Facts Tree" />
-          <h1>Facts Tree</h1>
-      </div>
-        <button className="btn btn-large btn-open">Share a Fact</button>
-    </header>
+    <>
+    {/* HEADER */}
+      <header className="header">
+        <div className="logo">
+            <img src="logo.png" alt="Facts Tree" />
+            <h1>{appTitle}</h1>
+        </div>
+          <button className="btn btn-large btn-open">Share a Fact</button>
+      </header>
+
+      <NewFactForm />
+
+      <main class="main">
+        <CategoryFilter />
+        <FactList />
+      </main>
+    </>
   );
+}
+
+function NewFactForm() {
+  return <form className="fact-form">Fact Form</form>;
+}
+
+
+function CategoryFilter() {
+  return <aside>Category Filter</aside>;
+}
+
+function FactList() {
+  return <section className="">Facts List</section>;
 }
 
 export default App;
