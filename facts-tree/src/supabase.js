84537aecaa9a9fc6fe9f3e3abb5751f2f4463815
zubbyzillion <princewillbeluonwuogbo1@gmail.com>
2023-09-02 04:30:35 +0100

    Connecting supaBase to App

--- facts-tree/src/supabase.js
+++ facts-tree/src/supabase.js
@@ -2,5 +2,5 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://ywrkgrgyvxhhejztanpt.supabase.co'
-const supabaseKey = process.env.SUPABASE_KEY
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl3cmtncmd5dnhoaGVqenRhbnB0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODY3MTkwMzAsImV4cCI6MjAwMjI5NTAzMH0.Vlj8SXhL2_KbF9VsypPX44w8fsKra5GCgIAz3C6qX2o'
 const supabase = createClient(supabaseUrl, supabaseKey)
