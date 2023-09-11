00ab10243bc16ee1d37cea402d79d154efdc7fd1
zubbyzillion <princewillbeluonwuogbo1@gmail.com>
2023-09-11 07:17:46 +0100

    Added horizontal scroll effect to facts section

--- facts-tree/src/App.js
+++ facts-tree/src/App.js
@@ -140,7 +140,7 @@ function isValidHttpUrl(string) {
 
 function NewFactForm({ setFacts, setShowForm }) {
   const [text, setText] = useState("");
-  const [source, setSource] = useState("http://example.com");
+  const [source, setSource] = useState("");
   const [category, setCategory] = useState("");
   const [isUploading, setIsUploading] = useState(false);
   const textLength = text.length;
